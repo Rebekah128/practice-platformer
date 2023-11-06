84099f3a1ada228df2a46f7d9a59c5b33942f913
Rebekah128 <142543687+Rebekah128@users.noreply.github.com>
2023-11-06 21:32:13 +1300

    Add files via upload

---
+++ ItemCollecter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollecter : MonoBehaviour
+{
+    private int Kiwi = 0;
+
+    [SerializeField] private Text kiwiText;
+
+    [SerializeField] private AudioSource collectionSoundEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Kiwi"))
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            Kiwi ++;
+            kiwiText.text = "Kiwifruit: " + Kiwi;
+        }
+    }
+}
